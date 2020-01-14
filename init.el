59a827d609c95d63e9a96235667d0055aac6311d
Harry Wright <harry@harrywright.net>
2020-01-14 16:54:32 +0000

    Move custom variables out of init.el. Hide menu, toolbar, etc.

--- init.el
+++ init.el
@@ -1,21 +1,2 @@
-
 (require 'org)
 (org-babel-load-file "~/.emacs.d/dotemacs.org")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doom-modeline-height 15 t)
- '(doom-modeline-icon t t)
- '(doom-modeline-major-mode-color-icon t t)
- '(doom-modeline-minor-modes t t)
- '(inhibit-compacting-font-caches t t)
- '(package-selected-packages (quote (neotree use-package delight))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
